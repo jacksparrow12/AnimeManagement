1b85ab0fba55891c6f45c64e4606e483e45cf527
jacksparrow12 <zenbonsakura@hotmail.de>
2014-07-13 17:21:06 +0200

    Parser to save the anime list to file

---
+++ Parser/SaveToFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+class SaveToFile
+{
+
+    AnimeList animeList = AnimeList.getInstance();
+    private static SaveToFile instance = new SaveToFile();
+
+    public static SaveToFile getInstance()
+    {
+        return instance;
+    }
+
+    public void saveAnimeListToFile(string path)
+    {
+        using (StreamWriter writer = (File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))
+        {
+            foreach (Anime anime in animeList.getAnimeList().Values)
+            {
+                writer.Write(anime.getTitle() + "#" + anime.buildStringOfFansubs() + "#" + anime.getEpisode() + "#" + anime.buildStringOfSub() + "#" + anime.buildStringOfVoice() + "#" + anime.buildStringOfSource() + "#" + anime.getDescription() + "#" + anime.getPathImg() + "++\n");
+            }
+        }
+
+    }
+}
+

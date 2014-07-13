ef6a8cae77df8480888dcb16a16e3ab797937908
jacksparrow12 <zenbonsakura@hotmail.de>
2014-07-13 17:19:09 +0200

    corrected a bug, if two animes from different fansub groups are available then it will jump in both folders

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -8,7 +8,6 @@ public class Parser
 {
 
     private string[] dirEntries;
-    private List<Anime> animeList = new List<Anime>();
     private static Parser instance = new Parser();
 
     public static Parser getInstance()
@@ -28,7 +27,7 @@ public class Parser
             {
                 if (depth < 1)
                 {
-                    processDirectory(dir, ++depth);             //Go one directory deeper
+                    processDirectory(dir, 1);             //Go one directory deeper
                 }
                 else if (depth == 1)
                 {
@@ -49,7 +48,7 @@ public class Parser
                         tmp.createAnimeObject(dir);
                     }
                 }
-                depth = 0;
+               
             }
         }
         catch (DirectoryNotFoundException e)
@@ -58,31 +57,4 @@ public class Parser
         }
     }
 
-    /*
-     * Returns all folders with full path.
-     */
-    public string[] getDirEntries()
-    {
-        return dirEntries;
-    }
-
-    /*
-     * Returns the anime list.
-     */
-    public List<Anime> getAnimeList()
-    {
-        return animeList;
-    }
-
-    /*
-    public static void Main()
-    {
-
-        Parser parser = Parser.getInstance();
-        AnimeList instance = AnimeList.getInstance();
-        parser.processDirectory("C:\\Users\\ipek\\Desktop\\testfolder", 0);
-        instance.printAllEntries();
-
-    }
-     * */
 }

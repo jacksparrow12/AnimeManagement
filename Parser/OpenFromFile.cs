ecb0186637c24df4ba5d832ae6f1f030a63b179e
jacksparrow12 <zenbonsakura@hotmail.de>
2014-07-13 17:17:36 +0200

    Read from file and creating anime list Not finished yet

---
+++ Parser/OpenFromFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+class OpenFromFile
+{
+    private static OpenFromFile instance = new OpenFromFile();
+    AnimeList animeList = AnimeList.getInstance();
+
+    public static OpenFromFile getInstance()
+    {
+        return instance;
+    }
+
+
+    public void readAnimesFromFile(string path)
+    {
+        using(StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            char[] delims = {'#'};
+            while((line  = reader.ReadLine())!=null)
+            {
+                 string[] entries = line.Split(delims);
+            }
+
+        }
+
+    }
+
+    public List<Anime> buildListFromStringArray()
+    {
+        return null;
+    }
+
+}
+

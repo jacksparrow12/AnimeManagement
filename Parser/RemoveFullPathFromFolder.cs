5ccb2089c63b0ecd3a994dc3fef51025a87b9687
jacksparrow12 <zenbonsakura@hotmail.de>
2014-07-17 00:57:51 +0200

    remove and add full path from media files

--- Parser/RemoveFullPathFromFolder.cs
+++ Parser/RemoveFullPathFromFolder.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 
-    class RemoveFullPathFromFolder
+class RemoveFullPathFromFolder
+{
+    private static RemoveFullPathFromFolder instance = new RemoveFullPathFromFolder();
+    private string path;
+    public static RemoveFullPathFromFolder getInstance()
     {
-
-        public static string getFolder(string path)
-        {
-            char[] delim = { '\\' };
-            return path.Split(delim).Last();
-        }
+        return instance;
     }
+
+
+    public static string getFolder(string path)
+    {
+        char[] delim = { '\\' };
+        return path.Split(delim).Last();
+    }
+
+    /*
+     *Add full path to media file
+     */
+    public string addFullPathToFile(string file)
+    {
+        return this.path +"\\"+ file;
+    }
+
+    /*
+     *Remove full path from all media files
+     *Store the parent directory of the media files to a variable
+     *return media files without the full path
+     */
+    public String[] removeFullPathFromFiles(string[] file)
+    {
+        this.path = Directory.GetParent(file[0]).FullName;
+        char[] delim = { '\\' };
+        for (int i = 0; i < file.Length; i++)
+        {
+            file[i] = file[i].Split(delim).Last();
+        }
+        return file;
+    }
+}

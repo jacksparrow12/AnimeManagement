bffca5438750103248d5166e35d605b8d3b5fed2
jacksparrow12 <zenbonsakura@hotmail.de>
2014-07-16 13:45:59 +0200

    getting the media files from the folder

---
+++ Parser/PathOfEpisode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+class PathOfEpisode
+{
+    public static String[] getPathOfEpisode(string path)
+    {
+        string[] entries = Directory.GetFiles(path);
+        List<string> mediaFiles = new List<string>();
+        foreach (string entry in entries)
+        {
+            if (entry.Contains(".mkv") || entry.Contains(".avi") || entry.Contains(".mp4"))
+            {
+                mediaFiles.Add(entry);
+            }
+
+        }
+        return mediaFiles.ToArray();
+
+    }
+}
+

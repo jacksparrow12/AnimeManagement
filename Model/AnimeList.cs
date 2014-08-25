a1f14121d4c3c3fc9e5dcb0fb4104df10db56ba8
jacksparrow12 <zenbonsakura@hotmail.de>
2014-08-25 10:34:36 +0200

    added filter

--- Model/AnimeList.cs
+++ Model/AnimeList.cs
@@ -9,12 +9,11 @@ class AnimeList
 {
     private Dictionary<string, Anime> animeList = new Dictionary<string, Anime>();
     private static AnimeList instance = new AnimeList();
-    private AnimeList() {}
+    public AnimeList() { }
     public static AnimeList getInstance()
     {
         return instance;
     }
-
 
     /*
      *Add an Anime to list. 
@@ -22,6 +21,31 @@ class AnimeList
     public void addAnimeToList(string title, int episode, string description, List<string> fansub, List<string> source, List<string> sub, List<string> voiceOutput, string pathImg, string[] mediaFiles)
     {
         animeList.Add(title, new Anime(title, episode, description, fansub, source, sub, voiceOutput, pathImg, mediaFiles));
+    }
+
+    public void addAnimeToList(Anime anime)
+    {
+        animeList.Add(anime.getTitle(), anime);
+    }
+
+    public void addAnimeToList(AnimeList filteredList)
+    {
+        for (int i = 0; i < filteredList.getAllTitle().Count(); i++)
+        {
+            if (!animeList.ContainsKey(filteredList.getAnimeList().Values.ToArray()[i].getTitle()))
+            {
+                animeList.Add(filteredList.getAnimeList().Values.ToArray()[i].getTitle(), filteredList.getAnimeList().Values.ToArray()[i]);
+            }
+
+        }
+    }
+
+    public void addAnimeToList(Dictionary<string, Anime> filteredList)
+    {
+        foreach (KeyValuePair<string, Anime> list in filteredList)
+        {
+            animeList.Add((string)list.Key, list.Value);
+        }
     }
 
     /*
@@ -69,11 +93,16 @@ class AnimeList
      */
     public void addImgPath(string title, string imgPath)
     {
-        if(animeList[title].getPathImg().Equals("no cover"))    //Check if no image path is set
+        if (animeList[title].getPathImg().Equals("no cover"))    //Check if no image path is set
         {
             animeList[title].setPathImg(imgPath);               //Set if no image path was set
         }
-        
+
+    }
+
+    public void addMediaFiles(string title, string[] mediaFiles)
+    {
+        animeList[title].setPathOfEpisode(mediaFiles);
     }
 
     /*
@@ -121,6 +150,143 @@ class AnimeList
     public void clearList()
     {
         animeList.Clear();
+    }
+
+    /*
+     *return anime with german subtitle
+     */
+    public AnimeList getGerSubAnime()
+    {
+        AnimeList gerSubAnime = new AnimeList();
+        foreach (Anime anime in animeList.Values)
+        {
+            if (anime.getSub().Count != 0)
+            {
+                foreach (String sub in anime.getSub())
+                {
+                    if (sub.Contains("Ger") || sub.Contains("German") || sub.Contains("GER") || sub.Contains("deutsch"))
+                    {
+                        gerSubAnime.addAnimeToList(anime);
+                    }
+                }
+            }
+        }
+
+        return gerSubAnime;
+    }
+
+    /*
+     *return anime with english subtitle
+     */
+    public AnimeList getEngSubAnime()
+    {
+        AnimeList engSubAnime = new AnimeList();
+        foreach (Anime anime in animeList.Values)
+        {
+            if (anime.getSub().Count != 0)
+            {
+                foreach (String sub in anime.getSub())
+                {
+                    if (sub.Contains("Eng") || sub.Contains("English") || sub.Contains("ENG") || sub.Contains("englisch"))
+                    {
+                        engSubAnime.addAnimeToList(anime);
+                    }
+                }
+            }
+
+        }
+
+        return engSubAnime;
+    }
+
+
+    /*
+     * 
+     * return german dub anime
+     */
+    public AnimeList getGerDubAnime()
+    {
+        AnimeList gerDubAnime = new AnimeList();
+        foreach (Anime anime in animeList.Values)
+        {
+            if (anime.getVoiceOutput().Count != 0)
+            {
+                foreach (String dub in anime.getVoiceOutput())
+                {
+                    if (dub.Contains("Ger") || dub.Contains("German") || dub.Contains("GER") || dub.Contains("deutsch"))
+                    {
+                        gerDubAnime.addAnimeToList(anime);
+                    }
+                }
+            }
+        }
+        return gerDubAnime;
+    }
+
+
+    /*
+     * return english dub anime
+     */
+    public AnimeList getEngDubAnime()
+    {
+        AnimeList engDubAnime = new AnimeList();
+        foreach (Anime anime in animeList.Values)
+        {
+            if (anime.getVoiceOutput().Count != 0)
+            {
+                foreach (String dub in anime.getVoiceOutput())
+                {
+                    if (dub.Contains("Eng") || dub.Contains("English") || dub.Contains("ENG") || dub.Contains("englisch"))
+                    {
+                        engDubAnime.addAnimeToList(anime);
+                    }
+                }
+            }
+
+        }
+        return engDubAnime;
+    }
+
+
+    /*
+     *return japanese dub anime
+     */
+    public AnimeList getJapDubAnime()
+    {
+        AnimeList japDubAnime = new AnimeList();
+        foreach (Anime anime in animeList.Values)
+        {
+            if (anime.getVoiceOutput().Count != 0)
+            {
+                foreach (String dub in anime.getVoiceOutput())
+                {
+                    if (dub.Contains("Jap") || dub.Contains("Japanese") || dub.Contains("Jap") || dub.Contains("japanisch"))
+                    {
+                        japDubAnime.addAnimeToList(anime);
+                    }
+                }
+            }
+        }
+        return japDubAnime;
+    }
+
+    public void removeAnimeWhichIsNotInTheList(AnimeList filteredList)
+    {
+        var list = animeList.Keys.Intersect(filteredList.getAnimeList().Keys);
+        List<string> keys = new List<string>();
+        foreach (KeyValuePair<string, Anime> entry in animeList)
+        {
+            if(!list.Contains(entry.Key))
+            {
+                keys.Add(entry.Key);
+            }
+        }
+
+        foreach (string key in keys)
+        {
+            animeList.Remove(key);
+        }
+          
     }
 }
 
